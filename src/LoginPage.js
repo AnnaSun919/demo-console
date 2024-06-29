1ef43b414c4505e5b736faa3c4f555ba4de0942a
annasun919 <yuval_sun@yahoo.com.hk>
2024-06-29 16:07:09 -0400

    add login pages

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
 
 export const LoginPage = () => {
   return <div>LoginPage</div>;
 };
+
+export default function AuthRouter() {
+  return (
+    <Routes>
+      <Route element={<Login />} path="/" />
+      <Route element={<Login />} path="/login" />
+    </Routes>
+  );
+}
