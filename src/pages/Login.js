bf681cb03d17136ed228c0cfb02aa9b66221f476
annasun919 <yuval_sun@yahoo.com.hk>
2026-01-26 13:30:05 -0500

    add login validate

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,34 +8,50 @@ import "./Login.css";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
 
   const dispatch = useDispatch();
 
   const handleLogin = (e) => {
     e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false || !username || !password) {
+      e.stopPropagation();
+      setValidated(true);
+      return;
+    }
+
     dispatch(login({ loginData: { username: username, password: password } }));
   };
 
   return (
     <div className="sign-in__wrapper">
-      <Form className="shadow p-4 bg-white rounded" onSubmit={handleLogin}>
+      <Form className="shadow p-4 bg-white rounded" noValidate validated={validated} onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Username</Form.Label>
           <Form.Control
-            type="username"
+            required
+            type="text"
             placeholder="Enter username"
             onChange={(e) => setUsername(e.target.value)}
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a username.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
+            required
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             autoComplete="on"
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a password.
+          </Form.Control.Feedback>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
