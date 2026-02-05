8e8b7873c76836c1e3136eb5a0214bf164aec7f2
annasun919 <yuval_sun@yahoo.com.hk>
2026-02-05 17:00:12 -0500

    remove unused code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import DemoApp from "./DemoApp";
 import { BrowserRouter } from "react-router-dom";
 import { useMemo, useReducer, createContext } from "react";
-import { reducer } from "./reducer";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
