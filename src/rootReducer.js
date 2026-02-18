a37ace5459c2ae21a2a56bc9774bbf04ef2bf867
annasun919 <yuval_sun@yahoo.com.hk>
2026-02-18 14:51:07 -0500

    add rootReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,10 +1,22 @@
 import { combineReducers } from "redux";
 import authReducer from "./reducers/Login";
 import bookingReducer from "./reducers/BookingListPage";
+import usersReducer from "./reducers/Users";
+import roomsReducer from "./reducers/Rooms";
+import groupsReducer from "./reducers/Groups";
+import approvalsReducer from "./reducers/Approvals";
+import myBookingsReducer from "./reducers/MyBookings";
+import bookRoomReducer from "./reducers/BookRoom";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   booking: bookingReducer,
+  users: usersReducer,
+  rooms: roomsReducer,
+  groups: groupsReducer,
+  approvals: approvalsReducer,
+  myBookings: myBookingsReducer,
+  bookRoom: bookRoomReducer,
 });
 
 export default rootReducer;
