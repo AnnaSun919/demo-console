4831c2016539e1ef0531ceafdbd7ea63c219f435
annasun919 <yuval_sun@yahoo.com.hk>
2026-02-23 15:43:24 -0500

    edit select drop down list style

--- src/pages/ManageRooms.js
+++ src/pages/ManageRooms.js
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { fetchRooms, createRoom, updateRoom, deleteRoom } from "../actions/Rooms";
 import { fetchGroups } from "../actions/Groups";
 
@@ -212,22 +213,18 @@ const ManageRooms = () => {
                         )}
 
                         {/* Dropdown to select groups */}
-                        <select
-                          className="flex h-10 w-full rounded-md border border-input bg-white px-3 py-2 text-sm "
-                          value=""
-                          onChange={(e) => {
-                            if (e.target.value) {
-                              handleAddGroup(e.target.value);
-                            }
-                          }}
-                        >
-                          <option value="">Select a group to add...</option>
-                          {availableGroups.map((group) => (
-                            <option key={group.groupId} value={group.groupId}>
-                              {group.name}
-                            </option>
-                          ))}
-                        </select>
+                        <Select onValueChange={(value) => handleAddGroup(value)}>
+                          <SelectTrigger className="w-full bg-white">
+                            <SelectValue placeholder="Select a group to add..." />
+                          </SelectTrigger>
+                          <SelectContent className="bg-white">
+                            {availableGroups.map((group) => (
+                              <SelectItem key={group.groupId} value={String(group.groupId)}>
+                                {group.name}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
                       </>
                     )}
                   </div>
